646fc9fbde6ff7ac13f3f15a611d988c5817cbfc
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-06-14 21:40:10 +0100

    added new projects, adjusted responsive layout

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const projectPicDisplay = document.getElementById('project_pic');
 const projectInfo = document.getElementById('project_info')
 const projectArray =[];
 
-function Projects(Name, Pic, Link, GitLink) { 
+function Projects(Name, Pic, Lang, Link, GitLink) { 
     this.name = Name;
-    this.pic = Pic;
+    this.pic = `./image/${Pic}`;
+    this.lang = Lang
     this.link = Link;
     this.gitLink = GitLink;
     
@@ -18,63 +19,81 @@ function Projects(Name, Pic, Link, GitLink) {
 const project1 = new Projects(
     "Dammy's Cakes World",
     "cakes.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/dammycakes/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/dammycakes");
 const project2 = new Projects(
-    "Furniture Site (Mobile view)",
+    "Mobile Furniture App",
     "furniture.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/furniture/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/furniture");
 const project3 = new Projects(
-    "Bookstore App (Mobile Only)",
+    "Mobile Bookstore App",
     "bookstore.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/bookstore/bookstore.html",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/bookstore/bookstore.html");
 const project4 = new Projects(
-    "Hotel Landing Page (Mobile and PC)",
+    "Hotel Landing Page",
     "hotel.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/hotel/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/hotel");
 const project5 = new Projects(
     "A Bunch Of JavaScript Calculators",
     "calculator.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/calculator/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/calculator");    
 const project6 = new Projects( 
     "Online Music Streaming App",
     "music.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/music-player/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/music-player");    
 const project7 = new Projects( 
     "Fashion Store",
     "shizzabelle.jpg",
+    ['html', 'css', 'js'],
     "https://nrg-portfolio.netlify.app/shizzabelle/",
     "https://github.com/NRG-BUILDS/portfolio/tree/main/shizzabelle");
 const project8 = new Projects(
     "Weather App",
     "weather.jpg",
+    ['html', 'css', 'js', 'api'],
     "https://nrg-builds.github.io/NRG-weather/",
     "https://github.com/NRG-BUILDS/NRG-weather");
 const project9 = new Projects(
         "Movie Information App",
         "movieroom.jpg",
+        ['html', 'css', 'js', 'api'],
         "https://nrg-builds.github.io/movie-app",
         "https://github.com/NRG-BUILDS/movie-app");
 const project10 = new Projects(
-    "Movie Information App (React)",
+    "Movie Information App",
     "movieroomReact.png",
+    ['react', 'css', 'js', 'api'],
     "https://nrg-movieroom.netlify.app",
     "https://github.com/NRG-BUILDS/movieroom");
 const project11 = new Projects(
-    "Space Agency App (React + Tailwind)",
+    "Space Agency App",
     "space.webp",
+    ['react', 'tailwind', 'api'],
     "https://nrg-space.netlify.app",
     "https://github.com/NRG-BUILDS/Space");
 const project12 = new Projects(
     "Responsive News Page",
     "news.jpg",
+    ['tailwind', 'js'],
     "https://nrg-builds.github.io/news-page",
     "https://github.com/NRG-BUILDS/news-page");
+const project13 = new Projects(
+    "Connect 4 game",
+    "connectgame.jpg",
+    ['react', 'tailwind', 'js'],
+    "https://nrg-connect4game.netlify.app",
+    "https://github.com/NRG-BUILDS/connect4game");
 function loadProjectListPc() { 
     let text = ""
     for (i = projectArray.length - 1; i > -1; i--) { 
@@ -89,12 +108,40 @@ function loadProjectListPc() {
 }    
 loadProjectListPc();
 
+function getLang(i) {
+    let tools = ``
+    if (projectArray[i].lang.includes('html')) {
+        tools += `<img src='./assets/html.svg' alt='html'>`
+    }
+    if (projectArray[i].lang.includes('css')) {
+        tools += `<img src='./assets/css.jpg'>`
+    }
+    if (projectArray[i].lang.includes('js')) {
+        tools += `<img src='./assets/js-icon.png'>`
+    }
+    if (projectArray[i].lang.includes('react')) {
+        tools += `<img src='./assets/react.png'>`
+    }
+    if (projectArray[i].lang.includes('tailwind')) {
+        tools += `<img src='./assets/tailwindcss.svg'>`
+    }
+    console.log(tools)
+    return tools
+}
+
 function previewProject(i) { 
     projectPicDisplay.style.animation = 'slideLeft 1s';
     projectPicDisplay.src = projectArray[i].pic;
-    projectInfo.innerHTML = `<p>${projectArray[i].name}</p>
+    projectInfo.innerHTML = `
+            <div>
+                <p>${projectArray[i].name}</p>
                 <a href='${projectArray[i].link}'><i class="bi bi-globe"></i></a>
-                <a href='${projectArray[i].gitLink}'><i class="bi bi-github"></i></a>`
+                <a href='${projectArray[i].gitLink}'><i class="bi bi-github"></i></a>
+            </div>
+            <div class='project_tools_img'>
+                ${getLang(i)}
+            </div>
+            `
 }
 
 function loadProjectListMobile() { 
@@ -103,10 +150,19 @@ function loadProjectListMobile() {
         text += `<div class='project_preview reveal'>
             <img src ="${projectArray[i].pic}">
             <div id='project_info'>
-                <p>${projectArray[i].name}</p>
-                <a href='${projectArray[i].link}'><i class="bi bi-globe"></i></a>
-                <a href='${projectArray[i].gitLink}'><i class="bi bi-github"></i></a>
-            </div>    
+                <div>
+                    <p>${projectArray[i].name}</p>
+                    <div class='link_pad'>
+                        <a href='${projectArray[i].link}'><i class="bi bi-globe"></i></a>
+                        <a href='${projectArray[i].gitLink}'><i class="bi bi-github"></i></a>
+                    </div>
+                    
+                </div>
+                <div class='project_tools_img'>
+                    ${getLang(i)}
+                </div> 
+            </div>
+            
             
         </div>`
     }
